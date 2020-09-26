28d2e32a14c280958d6b371e1c9f3730e2918932
TeuerStein <xenkai123456789@gmail.com>
2020-09-26 18:32:17 +0200

    Was added the new User model

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Seed(models.Model):
     ''' Seed model '''
 
--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfileInfo(models.Model):
+    ''' User model '''
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
+    portfolio_page = models.URLField(blank=True)
+    portfolio_picture = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.user.username
