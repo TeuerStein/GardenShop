dd464e56cbb45b75ebc8ad9be28801af255dc29b
TeuerStein <xenkai123456789@gmail.com>
2020-09-28 21:49:23 +0200

    Was added paginator for main shop pages

--- shop/services/shop_services.py
+++ shop/services/shop_services.py
@@ -1,4 +1,5 @@
 from shop.models import Seed, Rubric
+from django.core.paginator import Paginator
 
 
 def rubrics_for_shop_page(request):
@@ -10,14 +11,20 @@ def rubrics_for_shop_page(request):
 
 def take_objects_from_model_by_current_rubric(request, rubric_id):
     ''' Take objects from the Seed model
-    with filter by rubric and return context for a page '''
+    with filter by rubric and pagination
+    return context for a page '''
 
     objects = Seed.objects.filter(rubric=rubric_id)
+
+    paginator = Paginator(objects, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     rubrics = Rubric.objects.all()
     current_rubric = Rubric.objects.get(pk=rubric_id)
 
     context = {
-        'objects': objects,
+        'page_obj': page_obj,
         'rubrics': rubrics,
         'current_rubric': current_rubric,
     }
